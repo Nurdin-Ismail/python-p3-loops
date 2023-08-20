23a85b12145e6f11c87a4b55388721538ee804ce
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-08-20 21:47:10 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    count = 10
+    while count >= 0:
+        
+        print(count)
+        count = count - 1
+            
+        if count == 0:
+            print("Happy New Year!")
+            break
+        
+        
+happy_new_year()
+         
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [item ** 2 for item in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
